f601c1fe08651627d4525a0744d1d7b3db8c107e
Luis Medrano <homedirectory@Luiss-MacBook-Pro.local>
2023-04-06 14:31:14 -0500

    feat: add contact page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,10 @@
 import '@/styles/globals.css'
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import UserInfo from '@/components/UserInfo';
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
-      <UserInfo />
       <Component {...pageProps} />
     </UserProvider>
   );
